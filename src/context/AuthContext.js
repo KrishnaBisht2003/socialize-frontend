f10eb8de0344fe5b257cfbdc7d09c6984347d366
Krishna Bisht <122883169+KrishnaBisht2003@users.noreply.github.com>
2024-08-28 19:01:59 +0530

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,28 +2,28 @@ import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user: {
-    "_id": "64f6b914e339f5603df69cb5",
-    "username": "jane",
-    "email": "jane@gmial.com",
-    "profilePicture": "person/1.jpeg",
-    "coverPicture": "",
-    "followers": [
-      "64f6aed7511d118425ede4e2",
-      "64f6ade0dff5939a2f75798f"
-    ],
-    "following": [
-      "64f6ade0dff5939a2f75798f"
-    ],
-    "isAdmin": false,
-    "createdAt": "2023-09-05T05:13:56.951Z",
-    "updatedAt": "2023-09-06T18:44:24.452Z",
-    "__v": 0,
-    "desc": "hey its my updated description"
-  },
-  // user: JSON.parse(localStorage.getItem("user")) || null,
-  // isFetching: false,
-  // error: false,
+  // user: {
+  //   "_id": "64f6b914e339f5603df69cb5",
+  //   "username": "jane",
+  //   "email": "jane@gmial.com",
+  //   "profilePicture": "person/1.jpeg",
+  //   "coverPicture": "",
+  //   "followers": [
+  //     "64f6aed7511d118425ede4e2",
+  //     "64f6ade0dff5939a2f75798f"
+  //   ],
+  //   "following": [
+  //     "64f6ade0dff5939a2f75798f"
+  //   ],
+  //   "isAdmin": false,
+  //   "createdAt": "2023-09-05T05:13:56.951Z",
+  //   "updatedAt": "2023-09-06T18:44:24.452Z",
+  //   "__v": 0,
+  //   "desc": "hey its my updated description"
+  // },
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isFetching: false,
+  error: false,
 };
 
 
